7a179c21f8aaff1d40786dba022d1d9440668fa5
matrixd <matrixdaniil@gmail.com>
2013-10-19 02:05:28 +0400

    bug fix!

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ b = xlrd.open_workbook(xls)
 
 def create_table(rows, **kwargs):
     table = "\\begin{tabular}{ |"
-    width = kwargs.get('right')-kwargs.get('left')
+    width = kwargs.get('right')-kwargs.get('left')+1
     height = kwargs.get('bottom')-kwargs.get('top')
     for i in range(width):
         table += ' c | '
-    table += '}\n    '
+    table += '}\n   \\hline '
     for row in rows:
         #proccessing on table rows
         row = row[0]
@@ -25,14 +25,14 @@ def create_table(rows, **kwargs):
                     table += ' & '
                     ncell += 1
             ncell += 1
-        if row[-1][1] < kwargs.get('left'):
-            for k in range(kwargs.get('left') - row[-1][1]):
+        if row[-1][1] < kwargs.get('right'):
+            for k in range(kwargs.get('right') - row[-1][1]):
                     table += ' & '
         table = table[:-2]
         table += "\\" + "\\" + "\n    \\hline "  #row end
     #closing table
     table = table[:-10]
-    table += '\\end{tabular}'
+    table += '\\hline \\end{tabular}'
     print(table)
 
 
@@ -57,6 +57,10 @@ def read_table(book):
                     top = nrow
                     left = ncell
                     first = False
+                if top > nrow:
+                     top = nrow
+                if ncell < left:
+                    left = ncell
                 if ncell > right:
                     right = ncell
                 if nrow > bottom:
@@ -64,7 +68,6 @@ def read_table(book):
             ncell += 1
         if cell_list:
             rows.append((cell_list, nrow))
-        nrow += 1
 
     create_table(rows, left=left, right=right, top=top, bottom=bottom)
 
